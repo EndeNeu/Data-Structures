d7ded42084a231f62e7b215fd42943f24a017259
Emiliano Busiello <emilianobusiello@gmail.com>
2014-10-03 12:50:19 +0200

    [BST] made binary search nodes implement the deletable trait.

--- src/main/scala/com/ebusiello/fds/tree/binaryTree/search/AbstractBinarySearchNode.scala
+++ src/main/scala/com/ebusiello/fds/tree/binaryTree/search/AbstractBinarySearchNode.scala
@@ -1,5 +1,6 @@
 package com.ebusiello.fds.tree.binaryTree.search
 
+import com.ebusiello.fds.tree.Deletable
 import com.ebusiello.fds.tree.binaryTree.BasicBinaryNode
 import com.ebusiello.fds.tree.binaryTree.balanced.AbstractBalancedBinaryNode
 
@@ -7,11 +8,9 @@ import com.ebusiello.fds.tree.binaryTree.balanced.AbstractBalancedBinaryNode
  * This class is needed to have a common super type with a map method inside for both binary search nodes
  * and binary empty search nodes.
  */
-private[binaryTree] abstract class AbstractBinarySearchNode[T] extends BasicBinaryNode[T] {
+private[binaryTree] abstract class AbstractBinarySearchNode[T] extends BasicBinaryNode[T] with Deletable[T, AbstractBinarySearchNode] {
   def map[V](f: T => V): AbstractBalancedBinaryNode[V]
 
   def insert(mValue: T)(implicit ord: Ordering[T]): AbstractBinarySearchNode[T]
-
-  def delete(mValue: T)(implicit ord: Ordering[T]): AbstractBinarySearchNode[T]
 
 }
